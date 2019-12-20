362f40411b136ae3da830c8a45bda1e1cc0d5237
请喊我去编程 <43224864+CallMeToProgram@users.noreply.github.com>
2019-12-20 11:58:19 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,4 +1,14 @@
+# Author:    请喊我去编程
+# Bilibili:  https://space.bilibili.com/73503664
+# GitHub:    https://github.com/CallMeToProgram/Bad-Apple
+# Mail:      2395795924@qq.com
+
+# Created at 2019-06-17
+# Updated at 2019-12-20
+
 import matplotlib.pyplot as plt
+import cv2
+import pprint
 
 ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 char_len = len(ascii_char)
@@ -20,5 +30,24 @@ def convert(pic):
     return res
 
 
+def write(VIDEO_PATH):
+    print("转换视频中...")
+    vc = cv2.VideoCapture(VIDEO_PATH)
+    count = 0
+    f, frame = vc.read()
+    video_data = []
+    while f:
+        count += 1
+        print("\r转换进度: %d" % count, end='')
+        text = convert(frame)
+        video_data.append(text)
+        f, frame = vc.read()
+
+    with open("video_data.py", "w") as f:
+        f.write("video_data = " + pprint.pformat(video_data))
+
+    return video_data
+
+
 if __name__ == '__main__':
-    print(convert(plt.imread("miku.jpg")))
+    print(convert(plt.imread("1.jpg")))
